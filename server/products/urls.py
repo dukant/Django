b844dcb4a2a5c6953cba805dd68f86e51057422e
Ilia_Lomakin <ilyxaaxaa@mail.ru>
2018-12-20 16:06:42 +0300

    3 дз
    Дз к 3му уроку

--- server/products/urls.py
+++ server/products/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import list_view, detail_view
 
+app_name = "products"
+
 urlpatterns = [
-    path('<int:pk>/', detail_view),
-    path('', list_view),
+    path('<int:pk>/', detail_view, name="detail"),
+    path('', list_view, name="index"),
 ]
--- server/products/views.py
+++ server/products/views.py
@@ -1,21 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from .models import Product
 import json
 
 
 def list_view(request):
-    with open('data.json', 'r') as file:
-        context = json.load(file)
-
     return render(request, 'products\list.html', {
-        'products': context.get('products') or []
+        'products': get_list_or_404(Product)
     })
 
 
 def detail_view(request, pk):
-    with open('data.json', 'r') as file:
-        context = json.load(file)
-
-    products = context.get('products')
-
     return render(request, 'products\detail.html', {
-        'object': products[pk] if len(products) > pk else ""})
+        'object': get_object_or_404(Product, id=pk)})
